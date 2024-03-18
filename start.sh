3ac8157979dc64970a1fec885a9700b07aadfd08
BenTheBuilder <BenTheBuilder@cglabs.work>
2024-03-17 22:44:38 -0400

    fixing SSH Permissions

--- start.sh
+++ start.sh
@@ -6,14 +6,13 @@ echo ""
 echo ""
 echo "BenTheBuilder GitHub Action Runner: "
 echo "==================================================="
-echo ""
 echo "Configure SSH Directory"
-echo ""
 echo "TARGET REPO ${REPOSITORY}"
 
 # Set SSH Permissions
-chown -R docker /home/docker/.ssh
-chmod -R 600 /home/docker/.ssh
+chown docker /home/docker/.ssh
+chmod 0755 /home/docker/.ssh
+chmod -R 0600 /home/docker/.ssh/.
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
