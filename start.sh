ff1781d3f636afbdb00b54a024f5e88110fd1289
BenTheBuilder <BenTheBuilder@cglabs.work>
2024-03-19 14:11:27 -0400

    Working through Shell Scripting Logic..

--- start.sh
+++ start.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-REPOSITORY=$REPO
-ACCESS_TOKEN=$TOKEN
-SSH_TMP_DIR=$SSH_TMP_DIR #  /home/docker/tmp/
+REPOSITORY="$REPO"
+ACCESS_TOKEN="$TOKEN"
+SSH_TMP_DIR="$SSH_TMP_DIR" #  /home/docker/tmp/
  
 echo ""
 echo ""
 echo "${REPOSITORY}: GitHub Action Runner"
 echo "==================================================="
-echo "===CHECKING ENVIRONMENT VARIABLES==="
+echo "===|      CHECKING ENVIRONMENT VARIABLES       |==="
 echo "==================================================="
-echo ""
-echo ""
 echo "SSH_TMP_DIR: ${SSH_TMP_DIR}"
+echo "REPOSITORY: ${REPOSITORY}"
+echo "RUNNER_VERSION: ${RUNNER_VERSION}"     
+echo "OS_VERSION: ${OS_VERSION}"      
+echo ""
 
-if [ -d "$SSH_TMP_DIR"]; then
+if [ -d $SSH_TMP_DIR ]; then
     echo "SSH_TMP_DIR Found: ${SSH_TMP_DIR}"
     echo "Copying SSH Files from ${SSH_TMP_DIR}"
-    cp -a ${SSH_TMP_DIR}. /home/docker/.ssh
+    cp -a "${SSH_TMP_DIR}". /home/docker/.ssh
     cd /home/docker/.ssh
-    for key in *; do
-        if [!"authorized_keys"]; then
+    for key in *; 
+    do
+        if [ $key != "authorized_keys" ]; then
             echo "Setting Permissions for ${key}"
-            chmod 600 ${key}
+            chmod 600 "${key}"
         fi
     done
 fi
@@ -29,6 +32,7 @@ fi
 echo "Collecting Registration Token from GitHub"
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
+echo "${REG_TOKEN}"
 cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
